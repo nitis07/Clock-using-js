5bb80b5a8d141ab96380f65ae24a07ced5fbd3d0
nitis07 <nitishrai7280@gmail.com>
2024-09-21 21:34:32 +0530

    complete project

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Hello!');
+const time = document.querySelector(".time")
+const date = document.querySelector(".Date")
+
+setInterval((e) => {
+    let clock = new Date();
+    time.innerHTML = clock.toLocaleTimeString();
+    date.innerHTML = clock.toLocaleDateString();
+
+},1000);
